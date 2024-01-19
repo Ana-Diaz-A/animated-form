dedd5c2f93e887438ee1d835eb3b4de58c12f1be
Ana <116079787+Ana-Diaz-A@users.noreply.github.com>
2024-01-19 12:27:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+//Creates a new instance of Ractive associated with a DOM container, using a specific template and initial data.
+var ractive = new Ractive({
+  el: '#container',
+  template: '#template',
+  data: {}
+});
